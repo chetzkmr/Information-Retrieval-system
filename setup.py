6c46be936bc6f1720f9cf04c60a5e65cf66d8780
Chethan <chethan.kmr07@gmail.com>
2026-02-22 17:14:59 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Information-Retrieval-system',
+    version= '0.0.0',
+    author='Chethan Kumar',
+    author_email= 'chethan.kmr07@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+
+)
